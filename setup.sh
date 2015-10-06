ed34d2503c1d97ac079082af4ef4211979d3a1f0
Michael Sartain <mikesart@gmail.com>
2015-10-06 16:14:49 -0700

    Add script to copy files

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+copyit()
+{
+    local FILE="$@"
+
+    if [[ -f ~/$FILE ]]; then
+        echo "~/$FILE (skipping)"
+    else
+        cp $FILE ~/$FILE
+    fi
+}
+
+mkdir -p .config/htop
+
+copyit .bashrc
+copyit .bash_aliases
+copyit .vimrc
+copyit .gitconfig
+copyit .config/htop/htoprc
+
